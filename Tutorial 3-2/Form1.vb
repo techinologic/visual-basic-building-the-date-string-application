3540f8b0c4ad392f54d8b88b2475f57c81554b13
Paolo T. Inocencion <paoloinocencion@gmail.com>
2018-03-27 22:54:49 -0400

    Initial Commit

---
+++ Tutorial 3-2/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+
+
+    Private Sub btnShowDate_Click(sender As Object, e As EventArgs) Handles btnShowDate.Click
+        lblDateString.Text = txtDayOfWeek.Text & ", " & txtMonth.Text & " " & txtDayOfMonth.Text & ", " & txtYear.Text
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtDayOfMonth.Clear()
+        txtDayOfWeek.Clear()
+        txtMonth.Clear()
+        txtYear.Clear()
+        lblDateString.Text = String.Empty
+        ' Give the focus to txtDayOfWeek.'
+        txtMonth.Focus()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
